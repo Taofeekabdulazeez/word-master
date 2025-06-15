7d52c2c5317539c2d5a90f82bab0bebbc397d446
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-06-15 17:08:44 +0100

    refactor: standardize formatting and improve code readability in GameRoom class

--- server/src/models/game-room.ts
+++ server/src/models/game-room.ts
@@ -1,38 +1,40 @@
-import { Player } from "./player";
+import { Player } from './player';
 
 export class GameRoom {
-    private id: string;
-    private players: Map<string, Player> = new Map();
+  private id: string;
+  private players: Map<string, Player> = new Map();
 
-    constructor(id: string) {
-        this.id = id;
-    } 
+  constructor(id: string) {
+    this.id = id;
+  }
 
-    public getId(): string {
-        return this.id;
-    }
+  public getId(): string {
+    return this.id;
+  }
 
-    public getPlayers(): Player[] {
-        return GameRoom.sort(Array.from(this.players.values()));
-    }
+  public getPlayers(): Player[] {
+    return GameRoom.sort(Array.from(this.players.values()));
+  }
 
-    public addPlayer(name: string): void {
-        this.players.set(name, new Player(name));
-    }
+  public addPlayer(id: string): void {
+    this.players.set(id, new Player(id));
+  }
 
-    public getPlayer(name: string): Player {
-        return this.players.get(name)
-    }
+  public getPlayer(id: string): Player {
+    return this.players.get(id);
+  }
 
-    public removePlayer(name: string): void {
-        this.players.delete(name);
-    }
+  public removePlayer(id: string): void {
+    this.players.delete(id);
+  }
 
-    public hasPlayer(name: string): boolean {
-        return this.players.has(name);
-    } 
+  public hasPlayer(id: string): boolean {
+    return this.players.has(id);
+  }
 
-    public static sort(players: Player[]): Player[] {
-        return Array.from(players.values()).sort((a, b) => b.getPoints() - a.getPoints());
-    }
+  public static sort(players: Player[]): Player[] {
+    return Array.from(players.values()).sort(
+      (a, b) => b.getPoints() - a.getPoints(),
+    );
+  }
 }
